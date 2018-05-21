30a31c5458713293b6d5d1124eb1c89d5802191d
Daria Kalinina <lynxx91@mail.ru>
2018-05-21 15:20:16 +0300

    slider work with hover

---
+++ src/block/content/content.js
@@ -0,0 +1,6 @@
+console.log('я js');
+// let item = Array.from(document.querySelectorAll('.content__item'));
+// let itemWidth = 100 / item.length;
+// for(let i = 0; i < item.length; i++) {
+//   item[i].style.width = itemWidth+'%';
+// }
